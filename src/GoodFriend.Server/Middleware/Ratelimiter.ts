14a3e8b6324c4f5d0658e4c5df273a142e86779f
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-08-14 11:58:06 +0100

    fix: re-lower ratelimit

--- src/GoodFriend.Server/Middleware/Ratelimiter.ts
+++ src/GoodFriend.Server/Middleware/Ratelimiter.ts
@@ -2,6 +2,6 @@ import { rateLimit } from 'express-rate-limit';
 
 export default rateLimit({
   windowMs: 60 * 5000,
-  max: 1000,
+  max: 8,
   message: 'Too many requests from this IP, try again later',
 });
