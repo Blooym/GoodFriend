521ff0e0335d987aa3965b228ceb1507e4de33b9
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-08-25 01:20:48 +0100

    feat(server): add reqs per min to env

--- src/GoodFriend.Server/Middleware/Ratelimiter.ts
+++ src/GoodFriend.Server/Middleware/Ratelimiter.ts
@@ -4,7 +4,7 @@ import { rateLimit } from 'express-rate-limit';
  * Limits the number of requests per IP address.
  */
 export default rateLimit({
-  windowMs: 60 * 5000,
-  max: 12,
+  windowMs: 60 * 1000,
+  max: Number(process.env.REQS_PER_MIN) || 6,
   standardHeaders: true,
 });
