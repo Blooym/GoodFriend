57b48c4d4b15a42d2ded0d8d31bc1adeb9deb864
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2022-08-25 01:07:33 +0100

    feat: test experiemntal changes

--- src/GoodFriend.Server/Middleware/Ratelimiter.ts
+++ src/GoodFriend.Server/Middleware/Ratelimiter.ts
@@ -7,4 +7,8 @@ export default rateLimit({
   windowMs: 60 * 5000,
   max: 12,
   standardHeaders: true,
+  keyGenerator: (req) => {
+    const ip = req.headers['x-forwarded-for'] || req.ip;
+    return ip as string;
+  },
 });
