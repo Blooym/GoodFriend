c39f07d10f849f16c7cfab3527efee0995a2a744
BitsOfAByte <19539165+BitsOfAByte@users.noreply.github.com>
2023-06-07 07:05:43 +0000

    refactor(api): move static assets to /statc subpath

--- src/Api/src/api/static_content.rs
+++ src/Api/src/api/static_content.rs
@@ -4,7 +4,7 @@ use std::{borrow::Cow, ffi::OsStr, path::PathBuf};
 
 /// Returns a list of routes for the web API.
 pub fn routes() -> Vec<rocket::Route> {
-    routes![serve_static, index]
+    routes![serve_static, index, favicon]
 }
 
 /// Compiles the contents of the static directory into the binary at build-time
@@ -13,15 +13,22 @@ pub fn routes() -> Vec<rocket::Route> {
 #[folder = "./static/"]
 struct Asset;
 
-/// Displays the index.html page for the root path
+/// Serve index.html as the root path
 #[get("/")]
 pub async fn index() -> Option<RawHtml<Cow<'static, [u8]>>> {
     let asset = Asset::get("index.html")?;
     Some(RawHtml(asset.data))
 }
 
-/// Serves static assets that where bundled into the binary at build time.
-#[get("/<file..>")]
+/// Serve the favicon from the static files.
+#[get("/favicon.ico")]
+pub async fn favicon() -> Option<RawHtml<Cow<'static, [u8]>>> {
+    let asset: rust_embed::EmbeddedFile = Asset::get("favicon.ico")?;
+    Some(RawHtml(asset.data))
+}
+
+/// Serves static assets from the static directory or the application bundle.
+#[get("/static/<file..>")]
 pub async fn serve_static(file: PathBuf) -> Option<(ContentType, Cow<'static, [u8]>)> {
     let filename = file.display().to_string();
     let asset = Asset::get(&filename)?;
