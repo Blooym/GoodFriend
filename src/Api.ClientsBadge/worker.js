3bcee36a3aa3c8f3b86c46c1188b5f6909ef1faa
Blooym <19539165+Blooym@users.noreply.github.com>
2024-05-18 18:21:37 +0100

    feat(clientsbadge): enforce response caching server-side
    Signed-off-by: Blooym <19539165+Blooym@users.noreply.github.com>

--- src/Api.ClientsBadge/worker.js
+++ src/Api.ClientsBadge/worker.js
@@ -1,30 +1,42 @@
+const cache = caches.default;
+
 export default {
   async fetch(request, env, ctx) {
-    const response = await fetch(env.METADATA_URL, {
-      headers: { 
+    const cachedResponse = await cache.match(request);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+
+    const metadata = await fetch(env.METADATA_URL, {
+      headers: {
         "User-Agent": env.USER_AGENT,
         "X-Client-Key": env.CLIENT_KEY,
       },
     });
 
-    const json = await response.json()
+    const json = await metadata.json()
     if (!json.connections) {
-      return new Response(JSON.stringify({
+      const response = new Response(JSON.stringify({
         schemaVersion: 1,
         label: "Connected",
         message: "Unknown",
         color: "red",
         cacheSeconds: 180,
       }));
+      response.headers.set("Cache-Control", "max-age=180, must-revalidate, public");
+      await cache.put(request, response.clone());
     }
 
     const connectionsTotal = Object.values(json.connections).reduce((a, b) => a + b, 0);
-    return new Response(JSON.stringify({
+    const response = new Response(JSON.stringify({
       schemaVersion: 1,
       label: "Connected",
       message: parseInt(connectionsTotal).toLocaleString("en"),
       color: "brightgreen",
       cacheSeconds: env.CACHE_SECONDS || 3600,
     }));
+    response.headers.set("Cache-Control", `max-age=${env.CACHE_SECONDS || 3600}, must-revalidate, public`);
+    await cache.put(request, response.clone());
+    return response;
   },
 };
